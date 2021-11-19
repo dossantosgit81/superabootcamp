d7b4669d088c05ca5d6d527f7b37e4612dbe56e5
ps supera <pssupera@email.com>
2021-11-19 06:51:36 -0300

    Ajuste no endpoint auth

--- src/main/java/io/github/mendesrafael/rest/AutenticacaoResource.java
+++ src/main/java/io/github/mendesrafael/rest/AutenticacaoResource.java
@@ -7,6 +7,8 @@ import javax.annotation.security.PermitAll;
 import javax.inject.Inject;
 import javax.validation.ConstraintViolation;
 import javax.validation.Validator;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -34,14 +36,20 @@ public class AutenticacaoResource {
 	@PermitAll
 	@POST
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response autenticacao( AutenticacaoDTO autenticacaoDTO) {
-		 Set<ConstraintViolation<AutenticacaoDTO>> violations = validator.validate(autenticacaoDTO);
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public Response autenticacao(@FormParam("login") final String login, @FormParam("password") final String password ) {
+		AutenticacaoDTO autenticacaoDTO = new AutenticacaoDTO();  
+		autenticacaoDTO.setLogin(login);
+		autenticacaoDTO.setPassword(password);
+		
+		Set<ConstraintViolation<AutenticacaoDTO>> violations = validator.validate(autenticacaoDTO);
 		
 		    if (!violations.isEmpty()) {
 		    	var errors = violations.stream().map(e -> new String(e.getMessage())).collect(Collectors.toList());
 
 		        return Response.status(Status.FORBIDDEN).entity(new ObjectErro(errors)).build();
 		    } 
+		    
 		    var result = autenticacaoService.efetuarLogin(autenticacaoDTO);
 		    
 		    return Response.status(Status.OK).entity(result).build();
--- src/main/java/io/github/mendesrafael/rest/model/ResponseError.java
+++
@@ -1,5 +0,0 @@
-package io.github.mendesrafael.rest.model;
-
-public class ResponseError {
-
-}
