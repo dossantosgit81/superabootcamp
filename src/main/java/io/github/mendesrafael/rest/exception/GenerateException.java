c7e031ef54cdd247328353ca69e4b183ba1fc4bc
ps supera <pssupera@email.com>
2021-11-19 08:59:45 -0300

    Adicionando melhoria de tratamento de errors do sms

--- src/main/java/io/github/mendesrafael/rest/exception/GenerateException.java
+++ src/main/java/io/github/mendesrafael/rest/exception/GenerateException.java
@@ -8,6 +8,8 @@ import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
+import com.twilio.exception.ApiException;
+
 import io.github.mendesrafael.exceptions.CodigoNotValidException;
 import io.github.mendesrafael.exceptions.NotFindEntityException;
 import io.github.mendesrafael.rest.model.ObjectErro;
@@ -36,6 +38,9 @@ public class GenerateException implements ExceptionMapper<Exception> {
 			return Response.status(Status.NOT_FOUND).entity(objectErro).build();
 		}
 		
+		if(e instanceof ApiException) {
+			return Response.status(Status.INTERNAL_SERVER_ERROR).entity(new ObjectErro("Verifique as credenciais de envio de sms")).build();
+		}
 	
 		return Response.status(Status.INTERNAL_SERVER_ERROR).entity(new ObjectErro("Erro inesperado, me perdoe")).build();
 	}
